2fe8fdcb1a033f41c7df23ee30fbc98dad5ef292
DownRangeDave <timisjustin@gmail.com>
2022-03-19 04:20:48 -0600

    testsd
    s

--- Game/PlayerMovement.gd
+++ Game/PlayerMovement.gd
@@ -17,28 +17,31 @@ func _ready():
 
 
 func movement():
-	
+	var object = bullet.instance()
 	if Input.is_action_pressed("ui_right"):
-		$SpaceBody.set_translation(Vector3(($SpaceBody.get_translation().x)-speedmodifier,0,0))
+		$SpaceBody.set_translation(Vector3(($SpaceBody.get_translation().x)-speedmodifier,$SpaceBody.get_translation().y,0))
 	if Input.is_action_pressed("ui_left"):
-		$SpaceBody.set_translation(Vector3(($SpaceBody.get_translation().x)+speedmodifier,0,0))
+		$SpaceBody.set_translation(Vector3(($SpaceBody.get_translation().x)+speedmodifier,$SpaceBody.get_translation().y,0))
 	if Input.is_action_pressed("ui_up"):
-		$SpaceBody.set_translation(Vector3(0,($SpaceBody.get_translation().y)+speedmodifier,0))
+		$SpaceBody.set_translation(Vector3($SpaceBody.get_translation().x,($SpaceBody.get_translation().y)+speedmodifier,0))
 	if Input.is_action_pressed("ui_down"):
-		$SpaceBody.set_translation(Vector3(0,($SpaceBody.get_translation().y)-speedmodifier,0))
+		$SpaceBody.set_translation(Vector3($SpaceBody.get_translation().x,($SpaceBody.get_translation().y)-speedmodifier,0))
+	if Input.is_action_pressed("ui_right") and Input.is_action_pressed("ui_left"):
+		$SpaceBody.set_translation(Vector3(($SpaceBody.get_translation().x)-speedmodifier,$SpaceBody.get_translation().y,0))
+	if Input.is_action_pressed("ui_up") and Input.is_action_pressed("ui_down"):
+		$SpaceBody.set_translation(Vector3($SpaceBody.get_translation().x,($SpaceBody.get_translation().y)+speedmodifier,0))
 	if Input.is_action_pressed("ui_accept"):
-		
-		
-			var object = bullet.instance()
-			get_tree().get_root().add_child(object)
-			#object.set_translation = Vector3($SpaceBody.get_translation())
-			object.translation = Vector3($SpaceBody.get_translation().x-10,$SpaceBody.get_translation().y,
-			$SpaceBody.get_translation().z)
+			if $Timer.get_time_left() < .005:
+				
+				get_tree().get_root().add_child(object)
+			#object.set_translation = Vector3($SpaceBody.get_translation())                 
+				object.translation = Vector3($SpaceBody.get_translation().x-5,$SpaceBody.get_translation().y,
+				$SpaceBody.get_translation().z)
 	
-	if timershoot>=60:
-		timershoot=0
+			
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	
+	Autoload.playerPosition1 = $SpaceBody.get_translation()
+	Autoload.playerPosition2 = $SpaceBody.get_translation()
 	movement()
 	
